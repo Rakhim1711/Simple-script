1b841b35b40f8b77625c22d37e8def00f598377e
rakhimyakubjanov <rahim1711@gmail.com>
2022-02-17 23:17:54 -0500

    third commit

--- world_command.py
+++ world_command.py
@@ -1,1 +1,14 @@
 print("Hello World")
+
+def world():
+    print('Hi World')
+
+def new_function(string):
+
+    count = 0
+    for x in string:
+        if x == 'a':
+            count += 1
+    return count
+
+new_function('aaaabbbbbcccc')
